a8f461bc06aecfba972a8b5017c136a9be1e945d
BarrsX <xaionbarrs@gmail.com>
2020-12-18 16:44:32 -0500

    Removed use of os.

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import spotipy
 from lyricsgenius import Genius
-import os
 from spotipy.oauth2 import SpotifyOAuth
 
 # Replace with your genius token
-genius_token = 'genius token'
+genius_token = 'genius_token'
 
-# Set environment variable here
-os.environ['SPOTIPY_CLIENT_ID'] = 'client id'
-os.environ['SPOTIPY_CLIENT_SECRET'] = 'client secret'
-os.environ['SPOTIPY_REDIRECT_URI'] ='redirect URI'
-
-scope = 'user-read-currently-playing'
-
-# Get currently playing son from Spotify
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+# Get currently playing song from Spotify
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="client_id",
+                                               client_secret="client_secret",
+                                               redirect_uri="redirect_uri",
+                                               scope="scope"))
 current_song = sp.currently_playing()
 artist = current_song['item']['artists'][0]['name']
 title = current_song['item']['name']
@@ -25,5 +20,3 @@ genius = Genius(genius_token)
 # Display lyrics for the song
 song = genius.search_song(title=title, artist=artist)
 print(song.lyrics)
-
-
